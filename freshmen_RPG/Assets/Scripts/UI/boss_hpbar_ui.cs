c0f9010ce876ccdb80fc0ba44cbc7567fd899eb9
Gabin Lee <gb94@ewhain.net>
2024-06-18 03:38:47 +0900

    Feat: 족보 아이템-보스HP 반토막 구현

--- freshmen_RPG/Assets/Scripts/UI/boss_hpbar_ui.cs
+++ freshmen_RPG/Assets/Scripts/UI/boss_hpbar_ui.cs
@@ -44,4 +44,17 @@ public class boss_hpbar_ui : MonoBehaviour
             return;
         }
     }
+
+    public void CuttingBossHPHalf()
+    {
+        currHP /= 2;
+        UpdateHPBar();
+        if (currHP <= 0)
+        {
+            // 사망 코드
+            Debug.Log(gameObject.name + " is dead");
+            Destroy(gameObject); //이거 3d 리소스 찾으면 바꾸기
+            return;
+        }
+    }
 }
